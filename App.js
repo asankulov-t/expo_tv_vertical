8682c178afa043c36f983b7b62ccb82f8cc69bb1
unknown <asankulov-t>
2023-04-22 18:11:43 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from "react";
+import TimedSlideshow from "react-native-timed-slideshow";
 
 export default function App() {
+
+  let[data, setData]=useState()
+
+  useEffect(()=>{
+      fetch('https://64411cb4792fe886a89eaf4b.mockapi.io/tv_data')
+          .then((response)=>response.json())
+          .then((data)=>setData(data));
+
+  },[])
+  console.log(data)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {data&&<TimedSlideshow items={data}/>}
     </View>
   );
 }
